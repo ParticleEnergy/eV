91fb1809967efa115f4d01c9fd01a80a73ce18e0
绵羊 <ht.s.fog@gmail.com>
2022-02-11 13:23:19 +0800

    format

--- eV.Module/eV.EasyLog/Logger.cs
+++ eV.Module/eV.EasyLog/Logger.cs
@@ -10,49 +10,16 @@ namespace eV.EasyLog
     {
         private static ILog s_log = new Log();
 
-        public static void SetLogger(ILog log)
-        {
-            s_log = log;
-        }
-        public static void Debug(object message)
-        {
-            s_log.Debug(message);
-        }
-        public static void Debug(object message, Exception exception)
-        {
-            s_log.Debug(message, exception);
-        }
-        public static void Info(object message)
-        {
-            s_log.Info(message);
-        }
-        public static void Info(object message, Exception exception)
-        {
-            s_log.Info(message, exception);
-        }
-        public static void Warn(object message)
-        {
-            s_log.Warn(message);
-        }
-        public static void Warn(object message, Exception exception)
-        {
-            s_log.Warn(message, exception);
-        }
-        public static void Error(object message)
-        {
-            s_log.Error(message);
-        }
-        public static void Error(object message, Exception exception)
-        {
-            s_log.Error(message, exception);
-        }
-        public static void Fatal(object message)
-        {
-            s_log.Fatal(message);
-        }
-        public static void Fatal(object message, Exception exception)
-        {
-            s_log.Fatal(message, exception);
-        }
+        public static void SetLogger(ILog log) => s_log = log;
+        public static void Debug(object message) => s_log.Debug(message);
+        public static void Debug(object message, Exception exception) => s_log.Debug(message, exception);
+        public static void Info(object message) => s_log.Info(message);
+        public static void Info(object message, Exception exception) => s_log.Info(message, exception);
+        public static void Warn(object message) => s_log.Warn(message);
+        public static void Warn(object message, Exception exception) => s_log.Warn(message, exception);
+        public static void Error(object message) => s_log.Error(message);
+        public static void Error(object message, Exception exception) => s_log.Error(message, exception);
+        public static void Fatal(object message) => s_log.Fatal(message);
+        public static void Fatal(object message, Exception exception) => s_log.Fatal(message, exception);
     }
 }
