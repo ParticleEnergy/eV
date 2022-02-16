8afe89de36e2dd2dc2d28bfbcb90e30dbe67141d
绵羊 <ht.s.fog@gmail.com>
2022-02-16 16:26:05 +0800

    fix

--- eV.Network/eV.Network.Security.Client/DefaultSetting.cs
+++ eV.Network/eV.Network.Security.Client/DefaultSetting.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ParticleEnergy. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
-using System.Net.Sockets;
 using System.Security.Authentication;
 namespace eV.Network.Security.Client;
 
@@ -9,12 +8,9 @@ internal static class DefaultSetting
 {
     public const string Address = "127.0.0.1";
     public const int Port = 8888;
-    public const SocketType SocketType = (SocketType)1;
-    public const ProtocolType ProtocolType = (ProtocolType)6;
     public const int ReceiveBufferSize = 2048;
     public const SslProtocols SslProtocols = (SslProtocols)192;
     public const string TargetHost = "eV";
-    public const string CertFile = "";
 #if !NETSTANDARD
     public const int TcpKeepAliveTime = 60;
     public const int TcpKeepAliveInterval = 3;
