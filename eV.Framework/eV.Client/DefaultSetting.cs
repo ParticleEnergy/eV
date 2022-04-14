8d9f19b4bd82e2518fae937a21af51a8833b9399
绵羊 <ht.s.fog@gmail.com>
2022-04-14 19:01:54 +0800

    update

---
+++ eV.Framework/eV.Client/DefaultSetting.cs
@@ -0,0 +1,15 @@
+// Copyright (c) ParticleEnergy. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+namespace eV.Client;
+
+internal static class DefaultSetting
+{
+    public const string Logo = @"
+      _    __   _________            __
+  ___| |  / /  / ____/ (_)__  ____  / /_
+ / _ \ | / /  / /   / / / _ \/ __ \/ __/
+/  __/ |/ /  / /___/ / /  __/ / / / /_
+\___/|___/   \____/_/_/\___/_/ /_/\__/
+";
+}
