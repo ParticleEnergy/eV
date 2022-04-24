97133d5cedfeb58546619c92fc56fc3e5b2d28a1
绵羊 <ht.s.fog@gmail.com>
2022-04-24 23:34:12 +0800

    update

--- eV.Framework/eV.Server/Base/ModelBase.cs
+++ eV.Framework/eV.Server/Base/ModelBase.cs
@@ -1,11 +1,12 @@
 // Copyright (c) ParticleEnergy. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
+using eV.Server.Interface;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 namespace eV.Server.Base;
 
-public class ModelBase
+public class ModelBase : IModel
 {
     [BsonId]
     [BsonElement("_id")]
