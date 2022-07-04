8c1bb53bba12327bed7ed8a1b6e6a2934de77599
绵羊 <ht.s.fog@gmail.com>
2022-07-04 17:20:19 +0800

    update

--- eV.Example/eV.PublicObject/ClientObject/HelloClient.cs
+++
@@ -1,8 +0,0 @@
-using eV.Module.Routing.Attributes;
-namespace eV.PublicObject.ClientObject;
-
-[ClientMessage]
-public class ClientHelloMessage
-{
-    public string? Text { get; set; }
-}
--- eV.Example/eV.PublicObject/ServerObject/HelloServerMessage.cs
+++
@@ -1,8 +0,0 @@
-using eV.Module.Routing.Attributes;
-namespace eV.PublicObject.ServerObject;
-
-[ServerMessage]
-public class ServerHelloMessage
-{
-    public string? Text { get; set; }
-}
