5be8ecee13c2cef3f927bf82e40995971f3efacc
绵羊 <ht.s.fog@gmail.com>
2022-09-02 12:05:13 +0800

    update

--- eV.Framework/eV.Framework.Server/Properties/AssemblyInfo.cs
+++ eV.Framework/eV.Framework.Server/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using log4net.Config;
 [assembly: Guid("9a0fd4d8-150f-40fb-bb81-a7e539e42c2a")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyCompany("eV")]
+[assembly: AssemblyCompany("ParticleEnergy")]
 [assembly: AssemblyCopyright("Copyright (c) ParticleEnergy")]
 [assembly: AssemblyProduct("eV.Framework.Server")]
 [assembly: AssemblyTitle("eV.Framework.Server")]
