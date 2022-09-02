2f2e6e7eba36b799c177b6c6853610baffd71a49
绵羊 <ht.s.fog@gmail.com>
2022-09-02 11:38:12 +0800

    version

---
+++ eV.Framework/eV.Framework.Unity/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+// Copyright (c) ParticleEnergy. All rights reserved.
+// Licensed under the Apache license. See the LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: ComVisible(false)]
+[assembly: Guid("C4A74FDB-68E1-4561-B55B-A4569CC0AD43")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("eV")]
+[assembly: AssemblyCopyright("Copyright (c) ParticleEnergy")]
+[assembly: AssemblyProduct("eV.Framework.Unity")]
+[assembly: AssemblyTitle("eV.Framework.Unity")]
+[assembly: AssemblyDescription("Game Unity Framework")]
+[assembly: AssemblyVersion("1.4.12")]
+[assembly: AssemblyFileVersion("1.4.12")]
+[assembly: AssemblyInformationalVersion("1.4.12")]
